e9e96ebdf6e9386ce530cf7d1dab06e1e419cc74
Lauren Borland <lkborland@aol.com>
2018-06-20 14:39:51 -0500

    organize by Period, create boxplot for segment movements, fish1 trial1 and all fish trial1

--- ltraj_test.R
+++ ltraj_test.R
@@ -6,6 +6,7 @@ library(stats)
 library(plyr)
 library(dplyr)
 library(adehabitatHR)
+library(ggplot2)
 
 
 #carp data: type II
@@ -25,8 +26,8 @@ carp1
 
 #make data regular
 #date.ref1 <- as.POSIXct(c("2014-10-15 07:00:07", "2014-10-15 07:00:12", "2014-10-15 07:00:44", "2014-10-15 07:00:17", 
-                          "2014-10-15 07:00:07", "2014-10-15 07:00:13", "2014-10-15 07:00:10", "2014-10-15 07:00:20", 
-                          "2014-10-15 07:00:12", "2014-10-15 07:00:08"))
+#                          "2014-10-15 07:00:07", "2014-10-15 07:00:13", "2014-10-15 07:00:10", "2014-10-15 07:00:20", 
+#                          "2014-10-15 07:00:12", "2014-10-15 07:00:08"))
 #reg_c1 <- sett0(carp1, date.ref1, 3.5)
 
 
@@ -57,7 +58,7 @@ carp3 <- as.ltraj(xy = t3[,c("Easting","Northing")], date = dttrial3$d3,
 carp3
 
 
-#summarize carp by trial ? with basic trajectory calculations
+#summarize carp by trial with basic trajectory calculations
 #make df from carp1
 carp1df <- ldply(carp1)
 #make order of observations in order to join with infolocs
@@ -68,14 +69,44 @@ carp1info <- ldply(infolocs(carp1))
 #make order of observations in order to join with carp1df
 carp1info <- mutate(carp1info, num = 1:611514)
 carp1df <- left_join(carp1df, carp1info, by = "num")
+#organize by Period
+carp1df$Period <- factor(carp1df$Period, levels = c("PreCO2", "DuringCO2", "PostCO2"))
 
-#separate fish 1 from carp1df
+#separate fish 1 from carp1df as example
 carp1.1 <- carp1df[1:73039,]
+carp1.1$Period <- factor(carp1.1$Period, levels = c("PreCO2", "DuringCO2", "PostCO2"))
 
-#subset carp1.1 by Period
+#subset carp1.1 by Period as example
 carp1.1Pre <- filter(carp1.1, Period == "PreCO2")
 carp1.1During <- filter(carp1.1, Period == "DuringCO2")
 carp1.1Post <- filter(carp1.1, Period == "PostCO2")
+
+#create boxplot displaying movement segment distance
+#for fish 1 trial 1
+carp1.1mvmt <- ggplot(carp1.1, aes(x = Period, y = dt, fill = Period)) + 
+  geom_boxplot() +
+  scale_fill_manual("Period", values = c("sienna1", "sienna3", "sienna4")) +
+  theme_bw() +
+  ylab("Movement Segment Distance") +
+  xlab("Period of CO2 Treatment") +
+  theme(
+    strip.background = element_blank()
+  ) 
+#for all fish in trial 1, filter by segments shorter than 25
+carp1bp <- filter(carp1df, dt <= 25)
+carp1mvmt <- ggplot(carp1bp, aes(x = Period, y = dt, fill = Period)) + 
+  geom_boxplot() +
+  scale_fill_manual("Period", values = c("sienna1", "sienna3", "sienna4")) +
+  theme_bw() +
+  ylab("Movement Segment Distance") +
+  xlab("Period of CO2 Treatment") +
+  ggtitle("Distance of Segment Movement by Period\nTrial One") +
+  theme(
+    strip.background = element_blank()
+  ) 
+
+#create boxplot displaying relative angle 
+
 
 
 #null model (multiple)
